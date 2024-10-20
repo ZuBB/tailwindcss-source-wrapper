363b4e6474b220841773372d095f89ebac3bd091
Vasyl Zuziak <zvasylvv@gmail.com>
2024-10-20 12:39:51 +0200

    refactor: work with 'package.json' in a safer manner

--- src/print-help.js
+++ src/print-help.js
@@ -1,39 +1,45 @@
 import { readFileSync, realpathSync } from 'node:fs';
-import { dirname, resolve } from 'node:path';
+import { dirname, join } from 'node:path';
 import { argv } from 'node:process';
 
 const packageJsonFile = 'package.json';
 
-const getPackageVersion = () => {
+const getPackageJsonContent = () => {
   const binPath = realpathSync(argv[1]);
-  const packageJsonFilePath = resolve(dirname(binPath), '..', packageJsonFile);
+  console.log(binPath)
+  const packageJsonFilePath = join(dirname(binPath), packageJsonFile);
+  console.log(packageJsonFilePath)
 
   try {
-    return JSON.parse(readFileSync(packageJsonFilePath, 'utf8')).version;
-  } catch (_) {
-    throw new Error(`Can't process '${packageJsonFile}' file`);
+    return JSON.parse(readFileSync(packageJsonFilePath, 'utf8'));
+  } catch (error) {
+    throw new Error(`Can't process '${packageJsonFile}' file\n`, error);
   }
 }
 
-const getHelpMessage = (version) => {
+const getHelpMessage = () => {
+  const content = getPackageJsonContent();
+  const { version, bin } = content;
+  const cliName = Object.keys(bin)[0];
+
   return `
-tcw v${version}
+${cliName} v${version}
 
 Usage:
-   tcw [options...]
+    ${cliName} [options...]
 
 Options:
-   -w, --watch              pass '--watch' option to tailwindcss
-   -p, --poll               pass '--poll' option to tailwindcss
-       --content            pass '--content' option to tailwindcss
-       --postcss            Load custom PostCSS configuration
-   -m, --minify             pass '--minify' option to tailwindcss
-   -c, --config             pass '--config' option to tailwindcss
-       --no-autoprefixer    pass '--no-autoprefixer' option to tailwindcss
-   -h, --help               Display usage information
+    -w, --watch              pass '--watch' option to tailwindcss
+    -p, --poll               pass '--poll' option to tailwindcss
+        --content            pass '--content' option to tailwindcss
+        --postcss            Load custom PostCSS configuration
+    -m, --minify             pass '--minify' option to tailwindcss
+    -c, --config             pass '--config' option to tailwindcss
+        --no-autoprefixer    pass '--no-autoprefixer' option to tailwindcss
+    -h, --help               Display usage information
 `;
 }
 
-export default function () {
-  console.log(getHelpMessage(getPackageVersion()));
+export default function() {
+  console.log(getHelpMessage());
 };
